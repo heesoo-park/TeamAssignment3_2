5189c93c4f29200d9fa9db7f87c14737824b9e05
DESKTOP-8FKP09G\zaq86 <zaq8644@gmail.com>
2023-12-27 01:16:03 +0900

    DetailActivity 레이아웃

--- app/src/main/java/com/example/teamsns/DetailPageActivity.kt
+++ app/src/main/java/com/example/teamsns/DetailPageActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class DetailPageActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_page)
+
+        init()
+    }
+
+    private fun init(){
+
     }
 }
