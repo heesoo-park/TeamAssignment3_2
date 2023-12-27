b6595bd34b78c3b21ab4e38042765ddd2e57ed70
DESKTOP-8FKP09G\zaq86 <zaq8644@gmail.com>
2023-12-27 18:56:09 +0900

    editUser수정

--- app/src/main/java/com/example/teamsns/UserDatabase.kt
+++ app/src/main/java/com/example/teamsns/UserDatabase.kt
@@ -49,31 +49,31 @@ object UserDatabase {
 
     // 로그인한 사용자 저장 용도
     fun addUser(user: User) {
-        userList.add(user)
+        totalUserData.add(user)
     }
 
     // 사용자 리스트와 게시물 출력 용도
     fun getTotalUser(): ArrayList<User> {
-        return userList
+        return totalUserData
     }
 
     // 로그인한 사용자 정보 가져오는 용도
     fun getUser(id: String): User? {
-        return userList.find { it.id == id }
+        return totalUserData.find { it.id == id }
     }
 
     // 사용자 정보 수정 용도
     fun editUserData(user: User) {
-        userList.forEach {
-            it.name = user.name
-            it.id = user.id
+        totalUserData.find {it.id == user.id}.let {
+            it!!.name = user.name
             it.statusMessage = user.statusMessage
+            it.password = user.password
         }
     }
 
     // 회원가입 아이디 중복체크 용도
     fun checkUserId(id: String): Boolean {
-        userList.forEach {
+        totalUserData.forEach {
             if (it.id == id) return false
         }
         return true
@@ -81,7 +81,7 @@ object UserDatabase {
 
     // 로그인 아이디, 비밀번호 체크 용도
     fun matchUser(id: String, password: String): Boolean {
-        userList.forEach {
+        totalUserData.forEach {
             if (it.id == id && it.password == password) return true
         }
         return false
