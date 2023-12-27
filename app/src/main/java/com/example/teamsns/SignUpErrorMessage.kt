877139cec064b35252ceaf63c0e65187d074196e
Euny Kim <euny4535@gmail.com>
2023-12-27 12:28:00 +0900

    completed: - [x]  회원가입하고 돌아왔을 때 채워져있기 (⭐필수⭐) - [x]  비밀번호는 보안을 유지하기 위해 ***로 표시하기 (⭐필수⭐) - [x]  회원가입하고 돌아왔을 때 채워져있기 (⭐필수⭐) 로그인 버튼을 클릭하면 메인 페이지로 이동하거나 또는 오류 메세지 출력하기 (⭐필수⭐) - [x]  회원가입 버튼을 클릭하면 회원가입 페이지로 이동하기 (⭐필수⭐) - [x]  비밀번호와 비교하기 (⭐필수⭐)

---
+++ app/src/main/java/com/example/teamsns/SignUpErrorMessage.kt
@@ -0,0 +1,18 @@
+package com.example.teamsns
+
+import androidx.annotation.StringRes
+
+enum class SignUpErrorMessage(
+    @StringRes val message: Int,
+) {
+    EMPTY_NAME(R.string.empty_name_message),
+    EMPTY_ID(R.string.empty_id_message),
+    EMPTY_PASSWORD(R.string.empty_password_message),
+
+    INVIALID_NAME(R.string.name_error_message),
+    INVALID_ID(R.string.id_error_message),
+    INVALID_PASSWORD(R.string.password_error_message),
+
+    PASSWORD_MISMATCH(R.string.password_check_error_message),
+
+}
