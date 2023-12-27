64a3f23bd1b6e5c6c39fb5b78130a079955cf28e
DESKTOP-8FKP09G\zaq86 <zaq8644@gmail.com>
2023-12-27 18:55:45 +0900

    SignUpActivity 재활용

--- app/src/main/java/com/example/teamsns/SignUpActivity.kt
+++ app/src/main/java/com/example/teamsns/SignUpActivity.kt
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import java.util.regex.Pattern
+import com.example.teamsns.SignUpValidExtension.includeAlphabetAndNumber
+import com.example.teamsns.SignUpValidExtension.includeKorean
+import com.example.teamsns.SignUpValidExtension.includeSpecialCharacters
+import com.example.teamsns.SignUpValidExtension.includeUpperCase
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -26,40 +31,74 @@ class SignUpActivity : AppCompatActivity() {
     private val btnNext: Button by lazy {
         findViewById(R.id.btn_sign_up_next)
     }
+    private val tvId: TextView by lazy {
+        findViewById(R.id.tv_id)
+    }
+
     // arrays
     private val editTextArray: Array<EditText> by lazy {
         arrayOf(etName, etId, etPassword, etPasswordConfirmation)
     }
+
+    lateinit var userData: User
+    lateinit var id: String
+    lateinit var name: String
+    lateinit var status: String
+    lateinit var user: User
+    private fun setEditCheck() {
+        if (intent.getStringExtra("editId") != null) {
+            tvId.setText(R.string.edit_status)
+            etId.setHint(R.string.edit_status_message)
+            id = intent.getStringExtra("editId")!!
+
+            setEditUserData()
+        }
+    }
+
+    private fun setEditUserData() {
+        userData = UserDatabase.getUser(id)!!
+        name = userData.name
+        status = userData.statusMessage.toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         initView()
     }
+
     private fun initView() {
+        setEditCheck()
         setTextChangedListener()
         setOnFocusChangedListener()
         btnSignup()
     }
+
     private fun btnSignup() {
 
         btnNext.setOnClickListener {
-
-            val intent = Intent(this, ChooseProfileActivity::class.java).apply {
-                putExtra("id", etId.text.toString())
-                putExtra("password", etPassword.text.toString())
+            if (intent.getStringExtra("editId") != null) {
+                user = User(
+                    name = name,
+                    id = id,
+                    statusMessage = status,
+                    password = etPassword.toString()
+                )
+                UserDatabase.editUserData(user)
+            } else {
+                user = User(
+                    name = etName.toString(),
+                    id = etId.toString(),
+                    password = etPassword.toString(),
+                    statusMessage = ""
+                )
+                UserDatabase.addUser(user)
             }
-            val user = User(
-                name = etName.toString(),
-                id = etId.toString(),
-                password = etPassword.toString(),
-                profileImage = null,
-                statusMessage = null,
-                userPosts = null
-            )
-            UserDatabase.addUser(user)
-
+            val intent = Intent(this, ChooseProfileActivity::class.java).apply {
+                putExtra("id", user.id)
+                putExtra("password", user.password)
+            }
             setResult(RESULT_OK, intent)
-
             if (!isFinishing) finish()
 
         }
@@ -101,7 +140,7 @@ class SignUpActivity : AppCompatActivity() {
         if (etName.isVisible) {
             val errorCode = when {
                 text.isBlank() -> SignUpErrorMessage.EMPTY_NAME
-                (text.isNotBlank() && Pattern.matches("^[가-힣]*$",text)) -> null
+                text.includeKorean() -> null
 
                 else -> SignUpErrorMessage.INVIALID_NAME
             }
@@ -110,15 +149,17 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun getMessageValidId(): String? {
-        val text = etId.text.toString()
-        if (etId.isVisible) {
-            val errorCode = when {
-                text.isBlank() -> SignUpErrorMessage.EMPTY_ID
-                (text.isNotBlank() && Pattern.matches("^[a-z0-9]*\$",text)) -> null
+        if (intent.getStringExtra("editId") == null) {
+            val text = etId.text.toString()
+            if (etId.isVisible) {
+                val errorCode = when {
+                    text.isBlank() -> SignUpErrorMessage.EMPTY_ID
+                    text.includeAlphabetAndNumber() -> null
 
-                else -> SignUpErrorMessage.INVALID_PASSWORD
-            }
-            return errorCode?.let { getString(it.message) }
+                    else -> SignUpErrorMessage.INVALID_PASSWORD
+                }
+                return errorCode?.let { getString(it.message) }
+            } else return null
         } else return null
     }
 
@@ -128,7 +169,7 @@ class SignUpActivity : AppCompatActivity() {
         if (etPassword.isVisible) {
             val errorCode = when {
                 text.isBlank() -> getString(R.string.empty_password_message)
-                (text.isNotBlank() && Pattern.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{8,20}$", text)) -> null
+                text.includeSpecialCharacters() -> null
 
                 else -> getString(R.string.password_error_message)
             }
@@ -141,7 +182,10 @@ class SignUpActivity : AppCompatActivity() {
         if (etId.isVisible) {
             val errorCode = when {
                 text.isBlank() -> SignUpErrorMessage.EMPTY_PASSWORD
-                (text.isNotBlank() && Pattern.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{8,20}$", text)) -> null
+                (text.isNotBlank() && Pattern.matches(
+                    "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&.])[A-Za-z[0-9]$@$!%*#?&.]{8,20}$",
+                    text
+                )) -> null
 
                 (text != etPassword.text.toString()) -> SignUpErrorMessage.PASSWORD_MISMATCH
 
--- app/src/main/java/com/example/teamsns/User.kt
+++ app/src/main/java/com/example/teamsns/User.kt
@@ -2,9 +2,9 @@ package com.example.teamsns
 
 data class User(
     var name: String,
-    var id: String,
-    val password: String,
-    val profileImage: Int?, // 프로필 이미지 선택 화면으로 넘아가기 전에 프로필 이미지 수정
+    val id: String,
+    var password: String,
+    var profileImage: Int = (R.drawable.img_cat1), // 프로필 이미지 선택 화면으로 넘아가기 전에 프로필 이미지 수정
     var statusMessage: String?,
-    var userPosts: ArrayList<Post>? = arrayListOf()
+    var userPosts: ArrayList<Post> = arrayListOf()
 )
