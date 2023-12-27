e8db47fd8c3c1b982fd4bed90b621194288d9a61
DESKTOP-8FKP09G\zaq86 <zaq8644@gmail.com>
2023-12-27 10:41:35 +0900

    string

--- app/src/main/java/com/example/teamsns/DetailPageActivity.kt
+++ app/src/main/java/com/example/teamsns/DetailPageActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
