15415749740fb7a794b839cffd8dc22de29e347b
DESKTOP-8FKP09G\zaq86 <zaq8644@gmail.com>
2023-12-28 17:03:39 +0900

    profile img stroke

--- app/src/main/java/com/example/teamsns/MainPageActivity.kt
+++ app/src/main/java/com/example/teamsns/MainPageActivity.kt
@@ -3,6 +3,7 @@ package com.example.teamsns
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
@@ -75,8 +76,13 @@ class MainPageActivity : AppCompatActivity() {
             val profileView: View =
                 inflater.inflate(R.layout.profile_item, mainUserProfileList, false)
             val profileImg: ImageView = profileView.findViewById(R.id.iv_main_user_profile)
-
+            val profileStroke: ImageView = profileView.findViewById(R.id.iv_user_stroke)
             profileImg.setImageResource(user.profileImage)
+            if (loginUserId != user.id) {
+                profileStroke.setImageResource(R.drawable.selector_profile_image_background2)
+            }else {
+                profileStroke.setImageResource(R.drawable.selector_profile_image_background)
+            }
             mainUserProfileList.addView(profileView)
             setOnProflieClickListener(user, profileImg)
         }
